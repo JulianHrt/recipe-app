2f8b10455fdc4e43f3b61bd47e18e8d7dfda276e
Julian Hirt <julian.hirt94@web.de>
2022-10-27 12:15:01 +0200

    create Layout

---
+++ pages/components/Layout.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import styled from "styled-components";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
